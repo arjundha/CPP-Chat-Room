df1db07293111012ae61d3511b55d6a6d96e9a66
Arjun <59514930+arjundha@users.noreply.github.com>
2025-02-02 20:04:37 -0800

    renamed structs and added OwnedMessage and Connection

--- NetworkCommon/networkMessage.h
+++ NetworkCommon/networkMessage.h
@@ -4,22 +4,22 @@
 
 template <typename T>
 // Everything is public, so using structs instead of classes
-struct message_header {
+struct MessageHeader {
 	T id{};
 	uint32_t size = 0;
 };
 
 template <typename T>
-struct message {
-	message_header<T> header{};
+struct Message {
+	MessageHeader<T> header{};
 	std::vector<uint8_t> body;
 
 	size_t size() const {
-		return sizeof(message_header<T>) + body.size();
+		return sizeof(MessageHeader<T>) + body.size();
 	}
 
 	// std::cout override
-	friend std::ostream& operator << (std::ostream& os, const message<T>& message)
+	friend std::ostream& operator << (std::ostream& os, const Message<T>& message)
 	{
 		os << "Message ID:" << int(message.header.id) << " Size:" << message.header.size;
 		return os;
@@ -29,7 +29,7 @@ struct message {
 
 	// Push new data into a message
 	template<typename DataType>
-	friend message<T>& operator << (message<T>& mmessagesg, const DataType& data)
+	friend Message<T>& operator << (Message<T>& message, const DataType& data)
 	{
 		static_assert(std::is_standard_layout<DataType>::value, "Cannot push data into message vector, type is too complex");
 
@@ -46,7 +46,7 @@ struct message {
 
 	// Pull data out of a message
 	template<typename DataType>
-	friend message<T>& operator >> (message<T>& message, DataType& data)
+	friend Message<T>& operator >> (Message<T>& message, DataType& data)
 	{
 		static_assert(std::is_standard_layout<DataType>::value, "Cannot pull data into message vector, type is too complex");
 
@@ -61,4 +61,18 @@ struct message {
 		return message;
 	}
 
+};
+
+template <typename T>
+class Connection;
+
+template <typename T>
+struct OwnedMessage {
+	std::shared_ptr<Connection<T>> remote = nullptr;
+	Message<T> message;
+
+	friend std::ostream& operator << (std::ostream& os, const OwnedMesage<T>& message) {
+		os << message.message;
+		return os;
+	}
 };

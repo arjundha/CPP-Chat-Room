c9d1676392c1ec1132404609645aa1bbbdaf1ffe
Arjun <59514930+arjundha@users.noreply.github.com>
2025-02-02 20:04:00 -0800

    scaffolding for netowrkConnection.h

---
+++ NetworkCommon/networkConnection.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "networkCommon.h"
+#include "networkThreadSafeQueue.h"
+#include "networkMessage.h"
+
+template<typename T>
+class Connection : public std::enable_shared_from_this<Connection<T>> {
+public:
+	Connection() {
+
+	}
+
+	virtual ~Connection() {
+
+	}
+
+	bool connectToServer(); // called by clients
+	bool disconnect(); // called by clients or server
+	bool isConnected() const;
+
+	bool send(const message<T>& message);
+
+protected:
+	// ASIO stuff in here
+	asio::ip::tcp::socket m_socket; // Unique for each socket to a remote
+	asio::io_context&, m_asio_context; // shared across instance
+	ThreadSafeQueue<Message<T>> m_qMessagesOut; // messages to be sent to server from client
+	ThreadSafeQueue<OwnedMessage<T>>& m_qMessagesIn; // holds all messages received from server. NOTE: must provide a queue
+
+};

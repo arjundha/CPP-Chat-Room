f30af07f50ad7aa919555a2f832be785571d2ee7
Arjun <59514930+arjundha@users.noreply.github.com>
2025-02-02 18:31:38 -0800

    Created static lib for some helpers

---
+++ NetworkClient/client.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include <network.h>
+
+enum class CustomMessage : uint32_t {
+	Say,
+	Shout
+};
+
+int main() {
+	message<CustomMessage> message;
+	message.header.id = CustomMessage::Say;
+
+	//int a = 1;
+	//bool b = true;
+	//float c = 3.1415f;
+	//struct {
+	//	float x;
+	//	float y;
+	//} d[5];
+	//message << a << b << c << d;
+	//a = 99;
+	//b = false;
+	//c = 100.01f;
+	//message >> d >> c >> b >> a;
+	return 0;
+}
--- NetworkCommon/networkMessage.h
+++ NetworkCommon/networkMessage.h
@@ -2,7 +2,8 @@
 #include "networkCommon.h"
 
 
-template <typename T> 
+template <typename T>
+// Everything is public, so using structs instead of classes
 struct message_header {
 	T id{};
 	uint32_t size = 0;
@@ -15,6 +16,49 @@ struct message {
 
 	size_t size() const {
 		return sizeof(message_header<T>) + body.size();
+	}
+
+	// std::cout override
+	friend std::ostream& operator << (std::ostream& os, const message<T>& msg)
+	{
+		os << "Message ID:" << int(msg.header.id) << " Size:" << msg.header.size;
+		return os;
+	}
+
+	// Overloads for treating the body vector like a stack
+
+	// Push new data into a message
+	template<typename DataType>
+	friend message<T>& operator << (message<T>& msg, const DataType& data)
+	{
+		static_assert(std::is_standard_layout<DataType>::value, "Cannot push data into message vector, type is too complex");
+
+		size_t size = msg.body.size(); // current size
+
+		msg.body.resize(msg.body.size() + sizeof(DataType)); // resize for new data
+
+		std::memcpy(msg.body.data() + size, &data, sizeof(DataType)); // copy new data starting at end of old data
+
+		msg.header.size = msg.size(); // now update the size in the header
+
+		return msg;
+	}
+
+	// Pull data out of a message
+	template<typename DataType>
+	friend message<T>& operator >> (message<T>& msg, DataType& data)
+	{
+		static_assert(std::is_standard_layout<DataType>::value, "Cannot pull data into message vector, type is too complex");
+
+		size_t size = msg.body.size() - sizeof(DataType); // Size of data to be pulled out
+
+		std::memcpy(&data, msg.body.data() + size, sizeof(DataType)); // Copy the data out of message and into "data" var
+
+		// Update sizes
+		msg.body.resize(size);
+		msg.header.size = msg.size();
+
+		return msg;
 	}
 
 };
